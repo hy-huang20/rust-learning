9a56e8c0d5e5e0022983daca3dd3390a859076a4
hy-huang20 <1479054529@qq.com>
2025-06-24 22:01:39 +0800

    实现了最基础的 executor, 暂时未使用 Waker

--- async-await/readme.md
+++ async-await/readme.md
@@ -1,3 +1,3 @@
 # async-await
 
-一个 simple executor，[参考教程](https://os.phil-opp.com/zh-TW/async-await/#shi-xian)
+一个带 ``Waker`` 支持的简单 ``Executor``，[参考教程](https://os.phil-opp.com/zh-TW/async-await/#shi-xian)
--- async-await/src/main.rs
+++ async-await/src/main.rs
@@ -1,3 +1,20 @@
+use async_await::task::{Task, simple_executor::SimpleExecutor};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello World!");
+
+    let mut executor = SimpleExecutor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.run();
+
+    println!("It did not crash!");
+}
+
+async fn async_number() -> u32 {
+    42
+}
+
+async fn example_task() {
+    let number = async_number().await;
+    println!("async number: {}", number);
 }
