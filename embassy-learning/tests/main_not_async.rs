320e9bf38669037e7731791298e82c7e3ccf97e5
hy-huang20 <1479054529@qq.com>
2025-09-29 20:45:09 +0800

    test: 完善了 tests/ 文件夹下测例的使用方法

--- embassy-learning/tests/main_not_async.rs
+++ embassy-learning/tests/main_not_async.rs
@@ -2,18 +2,18 @@ use embassy_executor::{Spawner, main};
 
 
 /// 
+/// 运行：cargo test --test main_not_async
+/// 
 /// 预期结果：编译错误，提示 main function must be async
 /// 
 /// 测试在 wsl 上执行时是否确实会在编译期执行到
-/// embassy-executor-macros 中 main_std 函数
+/// embassy-executor-macros/src/lib.rs 中 main_std 函数
 /// 
-/// 如果执行 main_std 函数则一定会执行 main.rs 
-/// 中的 run 函数，而 run 函数针对一些非法情况
-/// 有相应的输出，输出在编译错误中
+/// 如果执行 src/lib.rs 中 main_std 函数则一定会执行 src/macros/main.rs 
+/// 中的 run 函数，而 run 函数针对一些非法情况有相应的输出，输出在编译错误中
 /// 
-/// 可以直接在 main_std 开头使用 println! 输出
-/// 自定义内容以验证。比如修改 src/lib.rs 中的
-/// main_std 为：
+/// 可以直接在 main_std 函数开头使用 println! 输出自定义内容以验证
+/// 比如修改 src/lib.rs 中的 main_std 为：
 /// 
 /// #[proc_macro_attribute]
 /// pub fn main_std(args: TokenStream, item: TokenStream) -> TokenStream {
--- embassy-learning/tests/task_not_async.rs
+++ embassy-learning/tests/task_not_async.rs
@@ -2,7 +2,7 @@ use embassy_executor::{Spawner, task, main};
 
 /// 预期结果：编译错误，提示 task functions must be async
 /// 
-/// embassy-executor-macros 中的 task 函数是平台无关的
+/// embassy-executor-macros/src/lib.rs 中的 task 函数是平台无关的
 
 #[task]
 fn run() {
