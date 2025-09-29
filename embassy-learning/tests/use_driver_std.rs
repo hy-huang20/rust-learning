426ebd843fa019190e8fda19de205158c1693c6c
hy-huang20 <1479054529@qq.com>
2025-09-29 20:45:09 +0800

    test: 完善了 tests/ 文件夹下测例的使用方法

--- embassy-learning/tests/use_driver_std.rs
+++ embassy-learning/tests/use_driver_std.rs
@@ -7,6 +7,8 @@ use embassy_time::{Timer};
 /// 测试在 wsl 上使用 embassy 使用的 time driver
 /// 是否是 driver_std
 /// 
+/// 位于 embassy-time/src/driver_std.rs
+/// 
 /// driver_std 实现了自己的 TimeDriver 类型并且
 /// 为其实现了 Driver trait。根据之前的代码分析，
 /// 调用者会调用自己实现的 time driver 类型中的
@@ -25,6 +27,7 @@ use embassy_time::{Timer};
 /// 
 /// 由于需要查看输出结果，所以运行 test 时需要加上 
 /// 参数 -- --nocapture 以显示输出结果
+/// 因为默认情况下，Cargo 会捕获所有测试中的标准输出
 /// 
 
 #[task]
